2a67b1bfa2a2c9ac144476c82bc25ac711a60171
mwina7 <155858128+mwina7@users.noreply.github.com>
2024-07-09 10:59:23 -0700

    Update Runner.java

--- FirstProject/Runner.java
+++ FirstProject/Runner.java
@@ -3,6 +3,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am updating this file");
-        System.out.println("Another Update");
+      
     }
 }
